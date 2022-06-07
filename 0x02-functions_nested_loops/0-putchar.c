42f923d96d68cc0c9fc90b05f5e523c9ff975975
lahat-m <lahatm.apach@gmail.com>
2022-06-07 09:02:52 -0700

    Print '_putchar' function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Write _putchar
+ * Description: This program print function to the screen
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+int ch;
+char content[9] = "_putchar";
+for (ch = 0; ch < 8; ch++)
+{
+_putchar(content[ch]);
+}
+_putchar('\n');
+
+return (0);
+}
