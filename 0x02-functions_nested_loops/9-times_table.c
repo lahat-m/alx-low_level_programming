bc32e590e9e497e04be22523a1ba6c57bc24e35a
lahat-m <lahatm.apach@gmail.com>
2022-06-07 18:44:06 -0700

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#incldue "main.h"
+/**
+ * times_table - print 9 times table
+ *
+ * Return: np return
+ */
+void times_table(void)
+	int a, b, c;
+	for (a = 0; a <= 9; a++)
+{
+	_putchar(48);
+	for (b = 1; b <= 9; b++)
+	{
+		c = a * b;
+		_putchar(44);
+		_putchar(32);
+		if (c <= 9)
+		{
+			_putchar(32);
+			_putchar(c + 48);
+		}
+		else
+		{
+			_putchar((C / 10) + 48);
+			_putchar((c % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
+}
