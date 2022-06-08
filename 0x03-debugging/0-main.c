e1ee001c559c38d27b4060114856e4253f75c2f0
lahat-m <lahatm.apach@gmail.com>
2022-06-08 13:57:21 -0700

    Avoid infinite loop

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,7 @@
 #include "main.h"
+
 /**
- *
- * main - test function that prints if an integer
- * is positive or negative
- *
+ * main - test function that prints if an integer is positive or negative
  * Return: 0
  */
 
