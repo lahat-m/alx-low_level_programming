b726722f5355a7994954f7c918fac0ed6aa09319
lahat-m <lahatm.apach@gmail.com>
2022-06-07 18:04:54 -0700

    Print every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int w, x, y, z;
+
+	for (w = 48; w <= 50; w++)
+	{
+		for (x = 48; x <= 50; x++)
+		{
+			for (y = 48; y <= 50; y++)
+			{
+				for (z = 48; z <= 50; z++)
+					break;
+				_putchar(w);
+				_putchar(x);
+				_putchar(58);
+				_putchar(y);
+				_putchar(z);
+				_putchar('\n');
+			}
+		}
+	}
+}
