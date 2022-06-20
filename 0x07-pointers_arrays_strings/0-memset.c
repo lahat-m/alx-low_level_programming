a50bc0638dae21f77a5d66f81ecf02f4c30ed8ca
lahat-m <lahatm.apach@gmail.com>
2022-06-20 08:57:19 -0700

    Fill memory wth a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	while (n > 0)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-		i++;
-		n--;
 	}
 	return (s);
 }
