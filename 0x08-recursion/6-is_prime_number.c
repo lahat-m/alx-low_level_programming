f9521a5deb8a301196e105ef9ac39390870f0f6d
lahat-m <lahatm.apach@gmail.com>
2022-06-22 14:29:33 -0700

    Return 1 if input is primer number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,47 @@
+nclude "main.h"
+
+
+
+int tmp_prime(int n, int i);
+
+
+
+/**
+ * divisors - prime?
+ * @n: number
+ * @m: integer params
+ *
+ * Return: boolean
+ */
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
+/**
+ * is_prime_number - prime
+ * @n: integer params
+ *
+ * Return: value
+ */
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
