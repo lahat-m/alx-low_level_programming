fa27887bcd7ddb4f70daae09c6ca69210e8f7b42
lahat-m <lahatm.apach@gmail.com>
2022-07-06 07:55:49 -0700

    Program that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,63 +1,61 @@
 #include <stdio.h>
 #include "3-calc.h"
-
 /**
- * op_add - calculates the sum of two integers.
- * @a: first integer value
- * @b: second integer value
+ * op_add - sum of two integer values.
+ * @a: first integer value.
+ * @b: second integer value.
  *
- * Return: sum of a and b
+ * Return: sum.
  */
 int op_add(int a, int b)
 {
-	    return (a + b);
-}
-
-
-/**
- * op_sub - difference of two integers.
- * @a: first integer value
- * @b: second integer value
- *
- * Return: difference of a and b
- */
-int op_sub(int a, int b)
-{
-	    return (a - b);
+	return (a + b);
 }
 
 /**
- * op_mul - product of two integers.
- * @a: first integer value
- * @b: second integer value
- *
- * Return: product of a and b
- */
-int op_mul(int a, int b)
-{
-	    return (a * b);
-}
-
-/**
- * op_div - calculates the division of two integers.
- * @a: 1st integer value
- * @b: 2nd integer value.
- *
- * Return: result of division; a and b
- */
-int op_div(int a, int b)
-{
-	    return (a / b);
-}
-
-/**
- * op_mod - remainder of division of integers.
+ * op_sub - difference of two integer values.
  * @a: first integer
  * @b: second integer
  *
- * Return: remainder of a divided b
+ * Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two integer values.
+ * @a: first integer value.
+ * @b: second integer value.
+ *
+ * Return: product.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two integer values.
+ * @a: first integer value.
+ * @b: second integer value.
+ *
+ * Return: division.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of division of two integer values.
+ * @a: first integer value
+ * @b: second integer value
+ *
+ * Return: remainder.
  */
 int op_mod(int a, int b)
 {
-	    return (a % b);
+	return (a % b);
 }
